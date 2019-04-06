31b773617c3527e49fbda071936e3fc95813cf61
JustinianoJL <jjluis91@gmail.com>
2019-04-06 21:31:58 +0200

    06-04-2019

--- ejemplo14springBootRestJPAData/src/main/java/com/curso/java/oo/spring/boot/model/entities/ClienteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.curso.java.oo.spring.boot.model.entities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente,Integer>
-{
-	//Code
-}
